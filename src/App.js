d90afd01431c64bcc545841fa52fc15173a2a870
Sourav R <developer.srv1@gmail.com>
2019-11-09 19:02:17 +0530

    using bs4, simple card show hide

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import Main from './Components/Main';
 
 function App() {
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+import cardImg from './../close-up-of-leaf.jpg'
 
 export default class Main extends Component {
+    state = {
+        show: false
+    }
+
     render() {
         return (
-            <Button variant="contained" color="primary">
-                Hello World
-            </Button>
+            <div className="row mt-5">
+                <div className="col">
+                    {this.state.show &&
+                        <div className="card" style={{width: '22rem', boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)"}}>
+                            <img src={cardImg} className="card-img-top" alt="" />
+                            <div className="card-body">
+                                <h5 className="card-title">Our Changing Planet</h5>
+                                <h6 class="card-subtitle mb-2 text-muted py-2" style={{fontSize: '95%'}}>by Kurt Wagner</h6>
+                                <p className="card-text text-muted">
+                                    Visit ten places on our planet that are undergoing the biggest changes today.
+                                </p>
+                                <div className="d-flex justify-content-between">
+                                    <a href="#" className="card-link" style={{flexGrow: 1}}>Card link</a>
+
+                                    <span className="d-flex justify-content-between align-items-center" style={{flexGrow: 0.5}}>
+                                        <i className="fa fa-heart-o" aria-hidden="true"></i>
+                                        <i className="fa fa-share-alt" aria-hidden="true"></i>
+                                        <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    }
+                </div>
+                <div className="col">
+                    <button className="btn btn-primary" type="button" onClick={() => this.setState((state) => ({show: !state.show}))}>Click</button>
+                </div>
+            </div>
         )
     }
 }
