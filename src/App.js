e69856d8a4273b3956d35c835461b8dee8f72026
Sourav R <developer.srv1@gmail.com>
2019-11-09 17:13:24 +0530

    create react app with basic material ui

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Main from './Components/Main';
+
+function App() {
+	return (
+		<div className="container">
+			<Main />
+		</div>
+	);
+}
+
+export default App;
