411259f8e7389b9aa9b63e66e2fd99889df56f20
Sourav R <developer.srv1@gmail.com>
2019-11-12 16:00:06 +0530

    using animate css to animate show hide element

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,8 +2,36 @@ import React, { Component } from 'react';
 import cardImg from './../close-up-of-leaf.jpg'
 
 export default class Main extends Component {
+    constructor(props) {
+        super(props);
+
+        this.cardRef = React.createRef();
+    }
+
     state = {
-        show: false
+        shouldShow: true,
+        show: true
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const targetElem = this.cardRef.current;
+        const reactThis = this;
+
+        if(prevState.shouldShow && ! this.state.shouldShow) {
+            if(targetElem) {
+                targetElem.classList.remove('zoomIn');
+                targetElem.classList.add('zoomOut', 'faster');
+                
+                targetElem.addEventListener('animationend', function() { 
+                    reactThis.setState({show: false});
+                });
+            }
+        }
+
+        if(! prevState.shouldShow && this.state.shouldShow) {
+            this.setState({show: true});
+        }
+        
     }
 
     render() {
@@ -11,11 +39,11 @@ export default class Main extends Component {
             <div className="row mt-5">
                 <div className="col">
                     {this.state.show &&
-                        <div className="card" style={{width: '22rem', boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)"}}>
+                        <div className="card animated zoomIn fast" ref={this.cardRef} style={{width: '22rem', boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)"}}>
                             <img src={cardImg} className="card-img-top" alt="" />
                             <div className="card-body">
                                 <h5 className="card-title">Our Changing Planet</h5>
-                                <h6 class="card-subtitle mb-2 text-muted py-2" style={{fontSize: '95%'}}>by Kurt Wagner</h6>
+                                <h6 className="card-subtitle mb-2 text-muted py-2" style={{fontSize: '95%'}}>by Kurt Wagner</h6>
                                 <p className="card-text text-muted">
                                     Visit ten places on our planet that are undergoing the biggest changes today.
                                 </p>
@@ -33,7 +61,7 @@ export default class Main extends Component {
                     }
                 </div>
                 <div className="col">
-                    <button className="btn btn-primary" type="button" onClick={() => this.setState((state) => ({show: !state.show}))}>Click</button>
+                    <button className="btn btn-primary" type="button" onClick={() => this.setState((state) => ({shouldShow: !state.shouldShow}))}>Click</button>
                 </div>
             </div>
         )
